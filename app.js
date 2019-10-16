78a6a2e51e22bdb1f400fc4e9da75e5e442ea71a
David Asem <ddy.asem@gmail.com>
2019-10-16 17:00:36 +0000

    adding function to determine the winner of the game

--- app.js
+++ app.js
@@ -41,4 +41,75 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     //next player
     nextPlayer();
   }
+
+
+
 });
+
+function nextPlayer() {
+  currentPlayer === 0 ? currentPlayer = 1 : currentPlayer = 0;
+  totalScores = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none'
+}
+
+
+document.querySelector('.btn-new').addEventListener('click', initialFunc);
+
+function initialFunc() {
+  scores = [0, 0];
+  currentPlayer = 0;
+  totalScores = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.getElementById('name-0').classList.remove('winner');
+  var elem = document.querySelector('#player-1-panel');
+  if (elem) {
+    elem.classList.remove('winner');
+  }
+  var elem1 = document.querySelector('player-0-panel');
+  if (elem1) {
+    elem1.classList.remove('active');
+  };
+  var elem2 = document.querySelector('player-1-panel');
+  if (elem2) {
+    elem2.classList.remove('active');
+  }
+  var elem3 = document.querySelector('player-0-panel');
+  if (elem3) {
+    elem3.classList.add('active');
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('#current-' + currentPlayer).textContent = dice;
+// //document.querySelector('#current-' + currentPlayer).innerHTML = '<em>' + dice + '</em>'; 
+
+// var u = document.querySelector('#score-0').textContent;
+// console.log(u)
